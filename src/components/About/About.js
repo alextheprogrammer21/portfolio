5ac08a07878d4f82b642ea18f68b30659df234fc
Alex <alextheprogrammer21@gmail.com>
2022-03-24 18:03:58 -0700

    first commit

---
+++ src/components/About/About.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { stackList } from "../../data/ProjectData";
+import {
+  Image,
+  Technologies,
+  Tech,
+  TechImg,
+  TechName,
+  ContactWrapper,
+} from "./AboutElements";
+import programmer from "../../Assets/programmer.png";
+function About() {
+  return (
+    <ContactWrapper id="about">
+      <div className="Container">
+        <div className="SectionTitle">About Me</div>
+        <div className="BigCard">
+          <Image src={programmer} alt="man-svgrepo" />
+          <div className="AboutBio">
+            Hello! My name is Alex. I'm a <strong>software engineer</strong>{" "}
+            with a background in accounting and finance looking to use my
+            programming experience to make an impact and help your organization,
+            the team, and the clients accomplish their development goals.
+          </div>
+          <div className="AboutBio tagline2">
+            I have become confident using the following technologies.
+          </div>
+          <Technologies>
+            {stackList.map((stack, index) => (
+              <Tech key={index} className="tech">
+                <TechImg src={stack.img} alt={stack.name} />
+                <TechName>{stack.name}</TechName>
+              </Tech>
+            ))}
+          </Technologies>
+        </div>
+      </div>
+    </ContactWrapper>
+  );
+}
+
+export default About;
